4e0dd4db52d0b4378cbc1f689eb1ff3a50ec0240
Zelest <hazjel@student.telkomuniversity.ac.id>
2026-03-11 02:01:50 +0700

    feat(fe): apply dark theme styling to store details page

--- fe-blue/src/views/admin/store/MyStore.vue
+++ fe-blue/src/views/admin/store/MyStore.vue
@@ -46,100 +46,75 @@ const mapSrc = computed(() => {
 
 <template>
   <div class="flex flex-col md:flex-row flex-1 gap-5">
-    <section
-      v-if="store"
-      class="flex flex-col w-full md:w-auto md:flex-1 h-fit rounded-[20px] p-5 gap-5 bg-white"
-    >
-      <p class="font-bold text-xl">Store Details</p>
+    <section v-if="store"
+      class="flex flex-col w-full md:w-auto md:flex-1 h-fit rounded-[20px] p-5 gap-5 bg-white dark:bg-surface-card">
+      <p class="font-bold text-xl dark:text-white">Store Details</p>
       <div class="flex flex-col md:flex-row items-start md:items-center w-full gap-5">
         <div class="flex items-center gap-[14px] w-full min-w-0">
           <div class="flex size-[84px] shrink-0 rounded-full bg-custom-background overflow-hidden">
             <img :src="store?.logo" class="size-full object-cover" alt="photo" />
           </div>
           <div class="flex flex-col gap-[6px] w-full overflow-hidden">
-            <p class="font-bold text-lg leading-tight w-full truncate">
+            <p class="font-bold text-lg leading-tight w-full truncate dark:text-white">
               {{ store.name }}
             </p>
             <p class="flex items-center gap-1 font-semibold text-custom-grey leading-none">
-              <img src="@/assets/images/icons/user-grey.svg" class="size-5" alt="icon" />
+              <img src="@/assets/images/icons/user-grey.svg" class="size-5 dark:invert" alt="icon" />
               {{ store.user?.name }}
             </p>
           </div>
         </div>
         <div class="flex items-center w-fit shrink-0 gap-1 ml-[98px] md:ml-0">
-          <img
-            v-if="store?.is_verified"
-            src="@/assets/images/icons/verify-star.svg"
-            class="flex size-6 shrink-0"
-            alt="icon"
-          />
+          <img v-if="store?.is_verified" src="@/assets/images/icons/verify-star.svg" class="flex size-6 shrink-0"
+            alt="icon" />
           <p v-if="store?.is_verified" class="font-bold text-custom-blue text-nowrap">VERIFIED</p>
           <p v-else class="font-bold text-custom-blue text-nowrap">NOT VERIFIED</p>
         </div>
       </div>
-      <hr class="border-custom-stroke" />
+      <hr class="border-custom-stroke dark:border-white/10" />
       <div class="flex flex-col gap-2">
-        <p class="font-bold text-xl">Store About</p>
-        <p class="font-semibold text-custom-grey whitespace-pre-wrap">{{ store?.about }}</p>
+        <p class="font-bold text-xl dark:text-white">Store About</p>
+        <p class="font-semibold text-custom-grey whitespace-pre-wrap dark:text-gray-400">{{ store?.about }}</p>
       </div>
       <div class="flex flex-col gap-3">
         <div class="flex items-center gap-[14px]">
-          <RouterLink
-            :to="getEditStoreRoute()"
-            class="flex items-center justify-center h-14 w-full rounded-2xl p-4 gap-2 bg-custom-black"
-          >
-            <img
-              src="@/assets/images/icons/edit-white.svg"
-              class="flex size-6 shrink-0"
-              alt="icon"
-            />
-            <span class="font-semibold text-white">Edit Store</span>
+          <RouterLink :to="getEditStoreRoute()"
+            class="flex items-center justify-center h-14 w-full rounded-2xl p-4 gap-2 bg-custom-black dark:bg-white dark:hover:bg-gray-200 transition-colors">
+            <img src="@/assets/images/icons/edit-white.svg" class="flex size-6 shrink-0 dark:brightness-0 dark:invert-0"
+              alt="icon" />
+            <span class="font-semibold text-white dark:text-black">Edit Store</span>
           </RouterLink>
         </div>
         <p class="flex items-center gap-2 font-semibold text-custom-grey leading-none">
-          <img
-            src="@/assets/images/icons/calendar-2-grey.svg"
-            class="size-6 flex shrink-0"
-            alt="icon"
-          />
+          <img src="@/assets/images/icons/calendar-2-grey.svg" class="size-6 flex shrink-0 dark:invert" alt="icon" />
           Created on {{ formatDate(store?.created_at) }}
         </p>
       </div>
     </section>
-    <section
-      v-if="store"
-      class="flex flex-col w-full md:w-[350px] lg:w-[400px] shrink-0 h-fit rounded-[20px] p-5 gap-5 bg-white"
-    >
-      <p class="font-bold text-xl">Store Address</p>
-      <div class="flex flex-col rounded-[20px] border border-custom-stroke p-4 gap-5">
+    <section v-if="store"
+      class="flex flex-col w-full md:w-[350px] lg:w-[400px] shrink-0 h-fit rounded-[20px] p-5 gap-5 bg-white dark:bg-surface-card">
+      <p class="font-bold text-xl dark:text-white">Store Address</p>
+      <div class="flex flex-col rounded-[20px] border border-custom-stroke p-4 gap-5 dark:border-white/10">
         <div class="flex items-center gap-[10px] w-full">
           <div
-            class="flex size-14 shrink-0 rounded-full bg-custom-icon-background overflow-hidden items-center justify-center"
-          >
-            <img
-              src="@/assets/images/icons/buildings-black.svg"
-              class="flex size-6 shrink-0"
-              alt="icon"
-            />
+            class="flex size-14 shrink-0 rounded-full bg-custom-icon-background dark:bg-white/5 overflow-hidden items-center justify-center">
+            <img src="@/assets/images/icons/buildings-black.svg"
+              class="flex size-6 shrink-0 dark:brightness-0 dark:invert" alt="icon" />
           </div>
           <div class="flex flex-col gap-1">
-            <p class="font-bold text-lg leading-none">{{ store?.city }}</p>
+            <p class="font-bold text-lg leading-none dark:text-white">{{ store?.city }}</p>
             <p class="font-semibold text-custom-grey">City Location</p>
           </div>
         </div>
-        <hr class="border-custom-stroke last:hidden" />
+        <hr class="border-custom-stroke last:hidden dark:border-white/10" />
         <div class="flex items-center gap-[10px] w-full">
           <div
-            class="flex size-14 shrink-0 rounded-full bg-custom-icon-background overflow-hidden items-center justify-center"
-          >
-            <img
-              src="@/assets/images/icons/keyboard-black.svg"
-              class="flex size-6 shrink-0"
-              alt="icon"
-            />
+            class="flex size-14 shrink-0 rounded-full bg-custom-icon-background overflow-hidden items-center justify-center dark:bg-white/5">
+            <img src="@/assets/images/icons/keyboard-black.svg"
+              class="flex size-6 shrink-0 dark:brightness-0 dark:invert" alt="icon" />
           </div>
           <div class="flex flex-col gap-1">
-            <p class="font-bold text-lg leading-none">{{ store?.postal_code }}</p>
+            <p class="font-bold text-lg leading-none dark:text-white">{{ store?.postal_code }}</p>
             <p class="font-semibold text-custom-grey">Post Code</p>
           </div>
         </div>
@@ -150,22 +125,19 @@ const mapSrc = computed(() => {
             <iframe class="size-full border-none" frameborder="0" :src="mapSrc"> </iframe>
           </div>
         </div>
-        <p v-if="fullAddress" class="font-semibold text-lg leading-[1.6em]">{{ fullAddress }}</p>
-        <p v-else class="font-semibold text-lg leading-[1.6em]">No address set</p>
+        <p v-if="fullAddress" class="font-semibold text-lg leading-[1.6em] dark:text-white">{{ fullAddress }}</p>
+        <p v-else class="font-semibold text-lg leading-[1.6em] dark:text-white">No address set</p>
       </div>
     </section>
-    <div v-else id="Empty-State-Store" class="flex flex-1 bg-white rounded-[20px] p-5">
+    <div v-else id="Empty-State-Store" class="flex flex-1 bg-white rounded-[20px] p-5 dark:bg-surface-card">
       <div
-        class="flex flex-col flex-1 items-center justify-center gap-4 rounded-2xl border-2 border-dashed border-custom-stroke p-5"
-      >
-        <img src="@/assets/images/icons/shop-remove-grey.svg" class="size-[52px]" alt="icon" />
+        class="flex flex-col flex-1 items-center justify-center gap-4 rounded-2xl border-2 border-dashed border-custom-stroke p-5 dark:border-white/10">
+        <img src="@/assets/images/icons/shop-remove-grey.svg" class="size-[52px] dark:invert" alt="icon" />
         <div class="flex flex-col gap-1 items-center text-center">
           <p class="font-semibold text-custom-grey">Oops, you don't have store profile</p>
         </div>
-        <RouterLink
-          :to="{ name: 'admin.create-store' }"
-          class="flex h-[52px] items-center rounded-2xl py-4 px-6 bg-custom-blue gap-[10px]"
-        >
+        <RouterLink :to="{ name: 'admin.create-store' }"
+          class="flex h-[52px] items-center rounded-2xl py-4 px-6 bg-custom-blue gap-[10px]">
           <span class="font-semibold text-lg text-white leading-none">Create Now +</span>
         </RouterLink>
       </div>
